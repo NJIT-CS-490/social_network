030f82dc889fd2dd6cb4e6547f400ff3acb1edf8
bziegler265 <brandt.ziegler@gmail.com>
2021-10-26 09:46:30 -0400

    refactored commit for heroku deployment

--- index.js
+++ index.js
@@ -20,10 +20,27 @@ mongoose.connect(process.env.MONGO_URL, {useNewUrlParser: true, useUnifiedTopolo
 });
 
 app.use("/images", express.static(path.join(__dirname, "public/images")));
+app.use(express.static(path.join(__dirname, "/client/build")));
+
 
 
 app.use(express.json());
-app.use(helmet());
+app.use(helmet.contentSecurityPolicy({
+  directives: {
+    defaultSrc: ["'self'"],
+    connectSrc: ["'self'"],
+    frameSrc: ["'self'"],
+    childSrc: ["'self'"],
+    scriptSrc: ["'self'"],
+    styleSrc: [
+      "'self'",
+      'https://fonts.googleapis.com'
+    ],
+    fontSrc: ["'self'"],
+    imgSrc: ["'self'"],
+    baseUri: ["'self'"],
+  },
+}));
 app.use(morgan("common"));
 
 const storage = multer.diskStorage({
@@ -35,14 +52,14 @@ const storage = multer.diskStorage({
     },
   });
   
-  const upload = multer({ storage: storage });
-  app.post("/api/upload", upload.single("file"), (req, res) => {
-    try {
-      return res.status(200).json("File uploaded successfully");
-    } catch (error) {
-      console.error(error);
-    }
-  });
+const upload = multer({ storage: storage });
+app.post("/api/upload", upload.single("file"), (req, res) => {
+  try {
+    return res.status(200).json("File uploaded successfully");
+  } catch (error) {
+    console.error(error);
+  }
+});
 
 app.use("/api/users", userRoute);
 app.use("/api/auth", authRoute);
@@ -50,7 +67,6 @@ app.use("/api/posts", postRoute);
 app.use("/api/conversations", conversationRoute);
 app.use("/api/messages", messageRoute);
 
-app.use(express.static(path.join(__dirname, "/client/build")));
 
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '/client/build', 'index.html'));
