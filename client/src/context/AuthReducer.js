8ccee5db2c000297905529ecb0d9521868ff9d05
bziegler265 <brandt.ziegler@gmail.com>
2021-10-26 09:20:42 -0400

    refactored commit for heroku deployment

--- client/src/context/AuthReducer.js
+++ client/src/context/AuthReducer.js
@@ -12,7 +12,7 @@ const AuthReducer = (state, action) =>{
                 isFetching: false,
                 error: false,
             };
-        case "LOGIN_FAIL":
+        case "LOGIN_FAILURE":
             return{
                 user: null,
                 isFetching: false,
